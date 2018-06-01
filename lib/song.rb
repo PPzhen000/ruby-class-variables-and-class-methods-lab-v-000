501a9b9b39219c8c805a0ec3b37a980cc1306352
Ariel Zhen <zhenpingping@live.com>
2018-06-01 06:53:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,43 @@
 class Song
 
-def initialize
+attr_accessor :name, :artist, :genre
+
+@@count = 0
+@@genres = []
+@@artists = []
+
+def initialize(name, artist, genre)
+  @name = name
+  @artist = artist
+  @genre = genre
+
+  @@count += 1
+  @@genres << genre if !@@genres.include?(genre)
+  @@artists << artist if !@@artists.include?(artist)
+end
+
+def count
+  @@count
+end
+
+def genres
+  @@genres
+end
+
+def artists
+  @@artists
+end
+
+def genre_count
+
+end
+
+def artist_count
 
 end 
+
+
+
+
 
 end
