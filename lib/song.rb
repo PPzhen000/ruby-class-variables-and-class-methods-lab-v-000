e45599076fc5ed42b9df094c2748f0de1a4724be
Ariel Zhen <zhenpingping@live.com>
2018-06-01 07:28:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,6 @@ end
 
 def self.genre_count
   genre_hash = {}
-  #binding.pry
   @@genres.each do |each_genre|
     if !genre_hash[each_genre]
     genre_hash[each_genre] = 1
@@ -39,13 +38,19 @@ def self.genre_count
     genre_hash[each_genre] = genre_hash[each_genre] + 1
     end
   end
-
   genre_hash
-
 end
 
 def self.artist_count
-
+  artist_hash = {}
+  @@artists.each do |each_artist|
+    if !artist_hash[each_artist]
+      artist_hash[each_artist] = 1
+    else
+      artist_hash[each_artist] += 1
+    end
+  end
+  artist_hash 
 end
 
 
